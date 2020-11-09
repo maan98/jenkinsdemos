1a2aa8dfc237ec0b93df8fc0db107dff84388abb
maan98 <60258550+maan98@users.noreply.github.com>
2020-11-08 23:11:27 -0800

    updated by another developer

--- ForGIT/src/main/java/com/dxc/controller/MyController.java
+++ ForGIT/src/main/java/com/dxc/controller/MyController.java
@@ -10,5 +10,11 @@ public class MyController {
 	public String sayHello() {
 		return "Hello from the controller";
 	}
+	
+	//updated by another developer
+	@GetMapping("/hi1")
+	public String sayHello() {
+		return "Hi from the controller";
+	}
 
 }
